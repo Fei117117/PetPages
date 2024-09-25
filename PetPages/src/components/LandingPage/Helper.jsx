cab18a67e0a0625399fd7104b70276ac6fac0168
Fei117117 <katherinekao@icloud.com>
2024-09-25 22:50:43 +0100

    pics

--- PetPages/src/components/LandingPage/Helper.jsx
+++ PetPages/src/components/LandingPage/Helper.jsx
@@ -1,11 +1,16 @@
+import helper1 from "../../assets/helper1.jpg";
+import helper2 from "../../assets/helper2.jpg";
+import helper3 from "../../assets/helper3.jpg";
+import helper4 from "../../assets/helper4.jpg";
+
 function Helper() {
   return (
     <div className="container">
       <h1>What is PetPages</h1>
-      <img src="../../assets/helper1.jpg" alt="Helper1" />
-      <img src="../../assets/helper2.jpg" alt="Helper2" />
-      <img src="../../assets/helper3.jpg" alt="Helper3" />
-      <img src="../../assets/helper4.jpg" alt="Helper4" />
+      <img src={helper1} alt="Helper1" />
+      <img src={helper2} alt="Helper2" />
+      <img src={helper3} alt="Helper3" />
+      <img src={helper4} alt="Helper4" />
     </div>
   );
 }
