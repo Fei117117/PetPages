6eb26b868d2f43aed72e2944216a36ecda33438b
Fei117117 <katherinekao@icloud.com>
2024-10-10 18:41:28 +0100

    mobile adjust

--- PetPages/src/components/LandingPage/Helper.jsx
+++ PetPages/src/components/LandingPage/Helper.jsx
@@ -1,19 +1,27 @@
-import helper1 from "../../assets/helper1.jpg";
-import helper2 from "../../assets/helper2.jpg";
-import helper3 from "../../assets/helper3.jpg";
-import helper4 from "../../assets/helper4.jpg";
+import "./landingPage.css";
+import { useNavigate } from "react-router-dom";
 
 function Helper() {
+  const navigate = useNavigate();
+  const goToLandingPage = () => {
+    navigate("/");
+  };
   return (
     <div className="page-background1">
       <div className="container">
         <h1 className="title-helper">What is PetPages</h1>
-        <div className="images">
-          <img src={helper1} alt="Helper1" className="helper-image" />
-          <img src={helper2} alt="Helper2" className="helper-image" />
-          <img src={helper3} alt="Helper3" className="helper-image" />
-          <img src={helper4} alt="Helper4" className="helper-image" />
-        </div>
+        <p id="text-helper">
+          Using a form of narrative therapy, PetPages provides you with prompts
+          to allow you & your loved ones to recall some of your most cherished
+          moments with your pet. Our service will guide you through this process
+          by checking-in & providing you with helpful resources & tips. Once
+          you're finished all of your chapters, we will send you a beautiful,
+          keepsake book. Your book will contain all of the stories & photos of
+          you and your pet to cherish... forever
+        </p>
+        <button className="helper" onClick={goToLandingPage}>
+          Back
+        </button>
       </div>
     </div>
   );
